44062ae90197c5000fd29154bbf24dd60cf41a69
李杰 <827188236@qq.com>
2024-01-30 17:51:02 +0800

    up

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       <div className='h-[60px] fixed top-0 left-0 flex items-center justify-between w-full z-40'>
         <Image src={`/home/Logo.png`} className="h-10 ml-5" alt="" />
         <Image src={`/home/head.png`} alt="" />
-        <Image src={`/home/start.png`} className="hover:opacity-70 cursor-pointer mr-5" alt="" onClick={() => router.push('/numerology')} />
+        <Image src={`/home/start.png`} className="hover:opacity-70 cursor-pointer mr-10" alt="" onClick={() => router.push('/numerology')} />
       </div>
       <div className='absolute left-0 top-0 w-full h-full z-30 overflow-auto'>
 
